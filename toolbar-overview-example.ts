2a9ac083b4c730920673a86f896f764c86005034
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-03-13 13:06:05 +0530

    Create toolbar-overview-example.ts

---
+++ toolbar-overview-example.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@Component({
+  selector: 'toolbar-overview-example',
+  templateUrl: 'toolbar-overview-example.html',
+  styleUrl: 'toolbar-overview-example.css',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+})
+export class ToolbarOverviewExample {}
